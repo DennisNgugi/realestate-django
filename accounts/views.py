1008a44363ef6c548733b2fd8f26ea9d1579a61c
Dennis Ngugi <denongugi73@gmail.com>
2019-12-12 19:54:30 -0800

    Normal user login

--- accounts/views.py
+++ accounts/views.py
@@ -45,10 +45,20 @@ def register(request):
 
 
 def login(request):
-        # if request.method == 'POST':
 
-            # register user
-            # return
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'Succesfully logged in!')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('login')
     return render(request, 'accounts/login.html')
 
 
